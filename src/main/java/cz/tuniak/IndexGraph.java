81e8a9cdd50880a15913a79868e11b217281a530
Patrik Sedliak <psedliak4@gmail.com>
2019-05-10 14:40:14 +0200

    parse Json using URL added

--- src/main/java/cz/tuniak/IndexGraph.java
+++ src/main/java/cz/tuniak/IndexGraph.java
@@ -9,7 +9,7 @@ import java.util.TreeMap;
 
 public class IndexGraph {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         JSONObject jsonDataObject = JsonHandler.parseJson();
         HashMap<Integer, HashMap<String, TreeMap<LocalDate, Double>>> data = loadData(jsonDataObject);
         UIHandler.createFrame(data);
--- src/main/java/cz/tuniak/JsonHandler.java
+++ src/main/java/cz/tuniak/JsonHandler.java
@@ -2,29 +2,40 @@ package cz.tuniak;
 
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 class JsonHandler {
 
     //Json object
-    static JSONObject parseJson() {
+    static JSONObject parseJson() throws Exception {
 
-        String filePath = "C:\\Users\\Tuna-NB\\IdeaProjects\\index-graph-eater\\src\\main\\java\\cz\\tuniak\\query.json";
+//        String filePath = "C:\\Users\\Tuna-NB\\IdeaProjects\\index-graph-eater\\src\\main\\java\\cz\\tuniak\\query.json";
+//
+//        StringBuilder valueBuilder = new StringBuilder();
+//        try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
+//            stream.forEach(s -> valueBuilder.append(s).append("\n"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        //creates and returns Json object
+//        return new JSONObject(valueBuilder.toString());
 
-        StringBuilder valueBuilder = new StringBuilder();
-        try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
-            stream.forEach(s -> valueBuilder.append(s).append("\n"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        URL url = new URL("https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=EUR&to_symbol=USD&apikey=demo");
+        URL anotherUrl = new URL("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&outputsize=full&apikey=demo");
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(url.openStream()));
 
-        //creates and returns Json object
-        return new JSONObject(valueBuilder.toString());
+        String inputLine;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((inputLine = bufferedReader.readLine()) != null)
+            stringBuilder.append(inputLine).append("\n");
+        bufferedReader.close();
 
+        return new JSONObject(stringBuilder.toString());
     }
+
 
 }
