a0c668efab51fccb2394b79389db9c7862dcb95c
Patrik Sedliak <48652510+tuniak437@users.noreply.github.com>
2019-05-16 21:09:59 +0200

    notes added (#4)
    * notes added
    
    * month tabs added
    
    * date data on x axis added
    
    * the hottest fix ever
    
    * parse Json using URL added

--- src/main/java/cz/tuniak/ChartHandler.java
+++ src/main/java/cz/tuniak/ChartHandler.java
@@ -6,21 +6,35 @@ import org.knowm.xchart.style.Styler;
 
 import java.awt.*;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.TreeMap;
 
 class ChartHandler {
-    static OHLCChart createChart(HashMap<Integer, HashMap<String, TreeMap<LocalDate, Double>>> data) {
+    //String = (open, high, low, close)
+    //LocalDate = full date in format "yyyy-MM-dd" || Double = values
+    static OHLCChart createChart(HashMap<String, TreeMap<LocalDate, Double>> month) {
         final OHLCChart chart = new OHLCChart(800, 600);
         chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideSE);
         chart.getStyler().setDefaultSeriesRenderStyle(OHLCSeries.OHLCSeriesRenderStyle.Candle);
+        //creates chart that contains four ArrayLists of values from certain month
+        ArrayList xData = new ArrayList(); // will fix later
+        month.get("open").forEach((keys, Double) -> {
+            //days counter
+//            YearMonth yearMonthObject = YearMonth.of(keys.getYear(), keys.getMonth());
+//            int daysInMonth = yearMonthObject.lengthOfMonth();
+            Date days = Date.from(keys.atStartOfDay(ZoneId.systemDefault()).toInstant());
+                xData.add(days);
+        });
 
         chart.addSeries("Eur/Usd",
-                new ArrayList<>(data.get(3).get("open").values()),
-                new ArrayList<>(data.get(3).get("high").values()),
-                new ArrayList<>(data.get(3).get("low").values()),
-                new ArrayList<>(data.get(3).get("close").values())
+                xData,
+                new ArrayList<>(month.get("open").values()),
+                new ArrayList<>(month.get("high").values()),
+                new ArrayList<>(month.get("low").values()),
+                new ArrayList<>(month.get("close").values())
         )
                 .setUpColor(Color.GREEN)
                 .setDownColor(Color.RED)
--- src/main/java/cz/tuniak/IndexGraph.java
+++ src/main/java/cz/tuniak/IndexGraph.java
@@ -1,7 +1,6 @@
 package cz.tuniak;
 
 import org.json.JSONObject;
-import org.knowm.xchart.OHLCChart;
 
 import java.time.LocalDate;
 import java.util.HashMap;
@@ -10,21 +9,27 @@ import java.util.TreeMap;
 
 public class IndexGraph {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         JSONObject jsonDataObject = JsonHandler.parseJson();
         HashMap<Integer, HashMap<String, TreeMap<LocalDate, Double>>> data = loadData(jsonDataObject);
-        OHLCChart chart = ChartHandler.createChart(data);
-        UIHandler.createFrame(chart);
+        UIHandler.createFrame(data);
+
     }
-    
+
     public static HashMap<Integer, HashMap<String, TreeMap<LocalDate, Double>>> loadData(JSONObject jsonDataObject) {
+        //creates new instance of DataHandler
         DataHandler myHandler = new DataHandler();
+        //gets variable JSONObject timeSeries
         JSONObject timeSeries = jsonDataObject.getJSONObject("Time Series FX (Daily)");
+        //iterates through the whole file taking keys of timeSeries object saving it in a String date variable
         for (Iterator<String> it = timeSeries.keys(); it.hasNext(); ) {
             String date = it.next();
 
+            //uses String date variable to get open, high, low, close values from JSONObject timeSeries
             myHandler.addNewEntry(date, timeSeries.getJSONObject(date));
         }
+        //returns HashMap with every value from selected month
         return myHandler.months;
     }
+
 }
